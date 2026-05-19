dbe1cf94b10f70d330df7b9798a508c0cd405db3
damiangr1986 <damiangr1986@gmail.com>
2026-05-18 23:52:01 -0300

    Clase Castillo Agregada

---
+++ src/juego/Castillo.java
@@ -0,0 +1,46 @@
+package juego;
+
+import java.awt.Color;
+import entorno.Entorno;
+
+public class Castillo {
+    private double x;
+    private double y;
+    private double ancho;
+    private double alto;
+
+    
+    public Castillo(double x, double y, double ancho, double alto) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
+
+    // Se dibuja al final del nivel
+    public void dibujar(Entorno e) {
+        e.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.GRAY);
+    }
+
+    
+   
+public boolean princesaWin(Princesa p) {
+    return p.bordeDerecho() >= this.bordeIzquierdo() && 
+           p.bordeIzquierdo() <= this.bordeDerecho() && 
+           p.bordeInferior() >= this.bordeSuperior() && 
+           p.bordeSuperior() <= this.bordeInferior();
+}
+
+
+    // Métodos de los bordes (Sensores de colisión)
+    public double bordeDerecho() { return this.x + this.ancho / 2; }
+    public double bordeIzquierdo() { return this.x - this.ancho / 2; }
+    public double bordeInferior() { return this.y + this.alto / 2; }
+    public double bordeSuperior() { return this.y - this.alto / 2; }
+
+    
+    public double getX() { return x; }
+    public void setX(double x) { this.x = x; }
+    public double getY() { return y; }
+    public void setY(double y) { this.y = y; }
+}
