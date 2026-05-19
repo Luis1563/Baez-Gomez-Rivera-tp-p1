9d940fc1a5e6a6fc6b455c2c2aa89f24c4790da8
damiangr1986 <damiangr1986@gmail.com>
2026-05-19 12:26:35 -0300

    Subiendo enemigo

---
+++ src/juego/Enemigo.java
@@ -0,0 +1,37 @@
+package juego;
+
+import java.awt.Color;
+import entorno.Entorno;
+
+public class Enemigo {
+    private double x;
+    private double y;
+    private double ancho;
+    private double alto;
+    private double velocidad;
+    private int direccion; // 1 = Derecha, -1 = Izquierda
+
+    // Constructor
+    public Enemigo(double x, double y, double ancho, double alto, double velocidad, int direccion) {
+        this.x = x;
+        this.y = y;
+        this.ancho = ancho;
+        this.alto = alto;
+        this.velocidad = velocidad;
+        this.direccion = direccion;
+    }
+
+    // Se dibuja en la pantalla
+    public void dibujar(Entorno e) {
+        e.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.BLUE);
+    }
+
+    // Movimiento horizontal autónomo
+    public void mover() {
+        this.x = this.x + (this.velocidad * this.direccion);
+    }
+       
+    public double getX() { return x; }
+    public void setX(double x) { this.x = x; }
+    public double getY() { return y; }
+}
