cbe497b7df1d951116dd08e59e5ab2cf2e9823e9
damiangr1986 <damiangr1986@gmail.com>
2026-05-20 07:46:26 -0300

    dibujo enemigos

--- src/juego/Enemigo.java
+++ src/juego/Enemigo.java
@@ -21,17 +21,53 @@ public class Enemigo {
         this.direccion = direccion;
     }
 
-    // Se dibuja en la pantalla
+    
     public void dibujar(Entorno e) {
         e.dibujarRectangulo(this.x, this.y, this.ancho, this.alto, 0, Color.BLUE);
     }
 
-    // Movimiento horizontal autónomo
+    
     public void mover() {
         this.x = this.x + (this.velocidad * this.direccion);
     }
+
+    public void actualizar(double velocidadMapa) {
+        this.mover();
+        this.x = this.x - velocidadMapa;
+    }
+
+    // public void esquivarIslas(Isla[] islas) {
+    //     for (Isla isla : islas) {
+    //         if (isla != null) {
+    //             if (this.colisionaConIsla(isla)) {
+    //                 this.direccion = this.direccion * -1; 
+    //             }
+    //         }
+    //     }
+    // }
+
+    public boolean colisionaConIsla(Isla isla) {
+        return this.bordeDerecho() >= isla.bordeIzquierdo() && 
+               this.bordeIzquierdo() <= isla.bordeDerecho() && 
+               this.bordeInferior() >= isla.bordeSuperior() && 
+               this.bordeSuperior() <= isla.bordeInferior();
+    }
+
+    public boolean colisionaConPrincesa(Princesa p) {
+        return this.bordeDerecho() >= p.bordeIzquierdo() && 
+               this.bordeIzquierdo() <= p.bordeDerecho() && 
+               this.bordeInferior() >= p.bordeSuperior() && 
+               this.bordeSuperior() <= p.bordeInferior();
+    }
+
+  
+    public double bordeDerecho() { return this.x + this.ancho / 2; }
+    public double bordeIzquierdo() { return this.x - this.ancho / 2; }
+    public double bordeInferior() { return this.y + this.alto / 2; }
+    public double bordeSuperior() { return this.y - this.alto / 2; }
        
     public double getX() { return x; }
+    public int getDireccion(){return direccion;}
     public void setX(double x) { this.x = x; }
     public double getY() { return y; }
 }
