a264cf9e8c4016fd4612008c6c1eb9ce75b6da01
Luis1563 <gomezlf@hotmail.com>
2026-06-02 22:21:42 -0300

    modificacion texto pantalla final

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -135,7 +135,7 @@ public class Juego extends InterfaceJuego
                 
                 // Configuramos la letra y el mensaje de victoria
                 this.entorno.cambiarFont("Arial", 45, Color.WHITE);
-                this.entorno.escribirTexto("¡Ganaste!", 350, 360);
+                this.entorno.escribirTexto("¡Ganaste!", 500, 360);
                 this.entorno.escribirTexto("Elizabeth libero a Mario!!!!", 350, 420); 
                 
                 return; // Corta el tick acá para congelar todo el juego al ganar
