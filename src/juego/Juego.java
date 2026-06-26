cd8f4ca1ff91bc1cda53215f3051e41e2d0a7898
damiangr1986 <damiangr1986@gmail.com>
2026-06-25 23:44:12 -0300

    cambio en el  Crearenemigonuevo

--- src/juego/Juego.java
+++ src/juego/Juego.java
@@ -521,7 +521,7 @@ public class Juego extends InterfaceJuego
             	x = entorno.ancho() + 60;
         	}
         	double y = buscarAlturaLibre();
-        	return new Enemigo(x, y, 45, 35, 2.0, direccion);
+        	return new Enemigo(x, y, 45, 35, this.velocidadMapa,direccion);
     	}
 
     	private double buscarAlturaLibre() {
